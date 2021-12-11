203d59b65dd2ca615a5e30bd6bb3876422e4d93a
Manoj M <manoj904378@gmail.com>
2021-12-11 10:44:53 +0530

    Create 06 Line Break in PHP.php

---
+++ PHP/Tips/06 Line Break in PHP.php
@@ -0,0 +1,8 @@
+<?php
+  /*
+  *   Format text content by replacing the new line (\n) with line break (<br/>) using nl2br() function in PHP.
+  */
+  echo "sdsd";
+  echo nl2br("\n");
+  echo "sdsdsd";
+?>
