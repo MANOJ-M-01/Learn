279394286ef1db591e0a7bea9ff7ee43ec8229f3
Manoj M <manoj904378@gmail.com>
2021-12-15 17:05:32 +0530

    mongoose insert find

--- NodeJS/03 Mongoose/server.js
+++ NodeJS/03 Mongoose/server.js
@@ -5,21 +5,104 @@ const http=require('http')
 const app=http.createServer()
 
 dotenv.config()
-connectDB()
-const playListSchema=new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
-    },
-    ctype:String,
-    videos:Number,
-    active:Boolean,
-    date:{
-        type:Date,
-        default:Date.now
-    }
-})
 
-const PlayList=mongoose.model('PlayList',playListSchema)
+// creaete collection
 
-app.listen(process.env.PORT,()=>{console.log(`Server running on port ${process.env.PORT}`)})
+// const playListSchema=new mongoose.Schema({
+//     name:{
+//         type:String,
+//         required:true
+//     },
+//     ctype:String,
+//     videos:Number,
+//     active:Boolean,
+//     date:{
+//         type:Date,
+//         default:Date.now
+//     }
+// })
+
+// const PlayList=mongoose.model('PlayList',playListSchema)
+
+
+
+// insert one
+var myobj = { name: "Company Inc", address: "Highway 37" }
+const set=async()=>{
+    await connectDB()
+    const connection =await mongoose.connection
+    let data=await connection.collection('playlists').insertOne(myobj)
+    console.log(data)
+}
+// set();
+
+
+// insert multiple
+let myobj2=([
+    {name: "Steve", address: "MTM"},
+    { name: "Anya", address: "KUZ" }
+ ])
+const setAll=async()=>{
+    await connectDB()
+    const connection =await mongoose.connection
+    var myobj = { name: "Company Inc", address: "Highway 37" }
+    let data=await connection.collection('playlists').insertMany(myobj2)
+    console.log(data)
+}
+// setAll();
+
+
+// find all
+const getAll=async()=>{
+    await connectDB()
+    const connection =await mongoose.connection 
+    let data=await connection.collection('playlists').find({}).toArray()
+    //console.log(data)
+    data.map(item=>{
+        console.log(item.name)
+        console.log(item.address)
+    })
+}
+
+// getAll();
+
+// https://stackoverflow.com/questions/33627238/mongoose-find-with-multiple-conditions
+// https://stackoverflow.com/questions/18969916/mongodb-sum-query
+// https://stackoverflow.com/questions/3305561/how-to-query-mongodb-with-like
+
+
+// find
+const get=async()=>{
+    await connectDB()
+    const connection =await mongoose.connection
+    // let data=await connection.collection('playlists').find({$or:[{name: "ram"},{address:"address"}]}).toArray()
+    let data=await connection.collection('playlists').find({name: "ram"}).toArray()
+    // let data=await connection.collection('playlists').find({name: /.*y.*/}).toArray()
+    // console.log(data)
+    data.map(item=>{
+        console.log(item.name)
+        console.log(item.address)
+    })
+}
+
+get();
+
+
+
+
+
+// connection.on('error', console.error.bind(console, 'connection error:'));
+// connection.once('open', function () {
+
+//     connection.db.collection("testing.playlists", function(err, collection){
+//         // collection.find({}).toArray(function(err, data){
+//         //     console.log(data); // it will print your collection data
+//         // })
+//         console.log("sd");
+//     });
+
+// });
+
+
+
+// app.listen(process.env.PORT,()=>{console.log(`Server running on port ${process.env.PORT}`)})
