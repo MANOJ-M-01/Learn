0691a478aacb17e90d596f0373e67a8a0a8ccde1
Manoj M <manoj904378@gmail.com>
2021-12-16 16:52:38 +0530

    js tip added

---
+++ JavaScript/01 Classes with Inheritance .js
@@ -0,0 +1,26 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
+
+class Animal {
+  constructor(name) {
+    this.name = name;
+  }
+
+  speak() {
+    console.log(`${this.name} makes a noise.`);
+  }
+}
+
+class Dog extends Animal {
+  constructor(name) {
+    super(name); // call the super class constructor and pass in the name parameter
+  }
+
+  speak() {
+    console.log(`${this.name} barks.`);
+  }
+}
+
+let d = new Dog('Mitzie');
+d.speak(); // Mitzie barks.
+
+
